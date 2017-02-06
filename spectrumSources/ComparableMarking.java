331a597a412ca7a498b8f9b2b24b33b690b90962
Anton Kovsharov <antonkovsharov@gmail.com>
2017-02-06 19:00:38 +0300

    comparable marking added

--- spectrumSources/ComparableMarking.java
+++ spectrumSources/ComparableMarking.java
@@ -4,9 +4,9 @@ import java.io.PrintWriter;
 import java.util.*;
 
 /**
- * Created by antonkov on 2/1/17.
+ * Created by antonkov on 6/2/17.
  */
-public class RandomMarking {
+public class ComparableMarking {
 
     final boolean USE_FILTER = true;
     Random rng = new Random(19);
@@ -29,10 +29,68 @@ public class RandomMarking {
         return hMarked;
     }
 
+    void dfs(int v, boolean[][] g, int[] d, int[] to) {
+        if (d[v] != -1)
+            return;
+        d[v] = 0;
+        to[v] = -1;
+        for (int u = 0; u < g.length; u++) {
+            if (g[v][u]) {
+                dfs(u, g, d, to);
+                if (d[u] + 1 > d[v]) {
+                    d[v] = d[u] + 1;
+                    to[v] = u;
+                }
+            }
+        }
+    }
+
+    ArrayList<int[][]> calcSamples(int[][][] mtx, long[][] specs) {
+        int n = mtx.length;
+        ArrayList<int[][]> samples = new ArrayList<>();
+        boolean[][] g = new boolean[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                boolean greater = false;
+                boolean less = false;
+                for (int ii = 0; ii < specs[i].length; ii++) {
+                    if (specs[i][ii] > specs[j][ii]) {
+                        greater = true;
+                        break;
+                    }
+                    if (specs[i][ii] < specs[j][ii]) {
+                        less = true;
+                    }
+                }
+                if (greater) {
+                    continue;
+                }
+                g[i][j] = less;
+            }
+        }
+        int start = 0;
+        int[] to = new int[n];
+        int[] d = new int[n];
+        Arrays.fill(d, -1);
+        for (int i = 0; i < n; i++) {
+            if (d[i] == -1) {
+                dfs(i, g, d, to);
+                if (d[i] > d[start]) {
+                    start = i;
+                }
+            }
+        }
+        while (start != -1) {
+            samples.add(mtx[start]);
+            start = to[start];
+        }
+        return samples;
+    }
+
     void run(String[] args) throws FileNotFoundException {
         int start = 0;
         if (args.length < 3) {
-            System.err.println("usage: RandomMarking [numberSamples] [M] [outFolder] [filenames]*");
+            System.err.println("usage: ComparableMarking [numberSamplesToChooseFrom] [M] [outFolder] [filenames]*");
             return;
         }
         int numberSamples = Integer.parseInt(args[start++]);
@@ -55,26 +113,27 @@ public class RandomMarking {
                     hd[i][j] = in.nextInt();
                 }
             }
-            int tried = 0;
+
+            int[][][] mtx = new int[numberSamples][][];
+            long[][] specs = new long[numberSamples][];
+            for (int i = 0; i < numberSamples; i++) {
+                mtx[i] = markMatrix(hd, M);
+                specs[i] = new TannerSpectrumFinderTable().solve(r,c, M, mtx[i]).spectrum;
+            }
+            ArrayList<int[][]> samples = calcSamples(mtx, specs);
+
             int sample = 0;
-            while (sample < numberSamples) {
-                tried++;
+            for (int[][] H : samples) {
                 String baseName = file.getName().split("\\.")[0];
                 String name = String.format("%s/%s_%03d.mtx", outFolder, baseName, sample);
-                int[][] hMarked = markMatrix(hd, M);
-                if (USE_FILTER) {
-                    TannerSpectrumFinderTable.SolveReport report = new TannerSpectrumFinderTable().solve(r, c, M, hMarked);
-                    if (!filter(report.spectrum)) {
-                        continue;
-                    }
-                }
+
                 try (PrintWriter out = new PrintWriter(name)) {
                     out.println("proto_matrix"); // matrix format
                     out.println(r + " " + c);
                     out.println(M);
                     for (int i = 0; i < r; i++) {
                         for (int j = 0; j < c; j++) {
-                            out.print(hMarked[i][j] + " ");
+                            out.print(H[i][j] + " ");
                         }
                         out.println();
                     }
@@ -85,6 +144,6 @@ public class RandomMarking {
     }
 
     public static void main(String[] args) throws FileNotFoundException {
-        new RandomMarking().run(args);
+        new ComparableMarking().run(args);
     }
 }
