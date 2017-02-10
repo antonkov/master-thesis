4e93edf08dc2f60c9699934993130d151eee50c0
Anton Kovsharov <antonkovsharov@gmail.com>
2017-02-10 08:28:54 +0300

    script fix

--- scripts/findTop.py
+++ scripts/findTop.py
@@ -5,7 +5,6 @@ def read_ssv(filename):
     return pd.read_csv(filename, delim_whitespace=True, header=1, index_col=False)
 
 name = '../reports/findTopRandomBase5_19'
-
 def read():
     with open(name) as f:
         cur_line = f.readline()
@@ -19,5 +18,8 @@ del df[0]
 del df[1]
 df.drop(df.index[df[3] == 'cont'], inplace=True)
 df.columns = ['filename', 'snr']
-df.index = df['filename']
+df = pd.concat([df['filename'].str.extract('b5_10_\d{4}_(?P<test>\d*)', expand=True),
+           df['snr']], axis=1)
+df = df.set_index('test')
+pd.options.display.max_colwidth = 100
 print df.sort_values(by='snr')
