1345a45aa69a8d2a8acc349a0d698cf11845f99d
Anton Kovsharov <antonkovsharov@gmail.com>
2017-06-04 03:34:09 +0300

    added thesis

--- scripts/snrAll.py
+++ scripts/snrAll.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 import pandas as pd
 import argparse
 import matplotlib.pyplot as plt
+import numpy as np
 from scipy.stats import t
 
 FER_THRESHOLD = 0.001
@@ -31,6 +32,11 @@ def calcSNR(xs, ys):
             return x0 + (x1 - x0) * frac
     return MAX_SNR + 0.1
 
+xgreen = []
+xred = []
+greenTests = []
+redTests = []
+
 filenames = args.folders # rr3_2304
 for filename in filenames:
     def read_ssv(f):
@@ -49,10 +55,8 @@ for filename in filenames:
     greenTestIds += tests[:len(tests)/2]
     redTestIds += tests[len(tests)/2:]
 
-    xgreen = []
-    xred = []
-    greenTests = []
-    redTests = []
+    cxgreen = []
+    cxred = []
     for name, group in df.groupby(['baseTest', 'markTest']):
         cur = group.loc[:, ['snr', 'fer']]
         cur.snr = cur.snr.astype(float)
@@ -62,48 +66,53 @@ for filename in filenames:
         ys = cur.fer
         snr = calcSNR(xs, ys)
         if name[0] in greenTestIds:
-            xgreen.append(snr)
+            cxgreen.append(snr)
             greenTests.append((xs,ys))
         elif name[0] in redTestIds:
-            xred.append(snr)
+            cxred.append(snr)
             redTests.append((xs,ys))
+    mean = np.mean(cxgreen+cxred)
+    cxgreen = np.array(cxgreen) - mean
+    cxred = np.array(cxred) - mean
+    xgreen += cxgreen.tolist()
+    xred += cxred.tolist()
 
-    def calcStudent(x1, x2):
-        n1 = len(x1)
-        n2 = len(x2)
-        M1 = sum(x1) / n1
-        M2 = sum(x2) / n2
-        S1 = sum((x-M1)**2 for x in x1)/n1
-        S2 = sum((x-M2)**2 for x in x2)/n2
-        # Satervait
-        for alpha in [0.2, 0.1, 0.05,0.01,0.001,0.0001]:
-            alpha /= 2
-            v1 = S1/n1
-            v2 = S2/n2
-            ta = (v1 * t.isf(alpha,n1-1) + v2 * t.isf(alpha,n2-1))/(v1+v2)
-            print ta
-            #print t.isf(alpha,n1+n2-2)
-        return (M1 - M2) / (S1/(n1-1) + S2/(n2-1))**0.5
+def calcStudent(x1, x2):
+    n1 = len(x1)
+    n2 = len(x2)
+    M1 = sum(x1) / n1
+    M2 = sum(x2) / n2
+    S1 = sum((x-M1)**2 for x in x1)/n1
+    S2 = sum((x-M2)**2 for x in x2)/n2
+    # Satervait
+    for alpha in [0.4,0.3,0.2, 0.1, 0.05,0.01,0.001,0.0001]:
+        alpha /= 2
+        v1 = S1/n1
+        v2 = S2/n2
+        ta = (v1 * t.isf(alpha,n1-1) + v2 * t.isf(alpha,n2-1))/(v1+v2)
+        print 2*alpha, ta
+        #print t.isf(alpha,n1+n2-2)
+    return (M1 - M2) / (S1/(n1-1) + S2/(n2-1))**0.5
 
-    tStudent = calcStudent(xred, xgreen)
-    print 'tStudent', tStudent
+tStudent = calcStudent(xred, xgreen)
+print 'tStudent', tStudent
 
-    f, ax = plt.subplots(figsize=(7, 7))
-    ax.set(yscale='log')
-    ax.set_xlim([1, MAX_SNR])
-    for xs, ys in greenTests:
-        ax.plot(xs, ys, color='green', lw=0.2)
-    for xs, ys in redTests:
-        ax.plot(xs, ys, color='red', lw=0.2)
+f, ax = plt.subplots(figsize=(7, 7))
+ax.set(yscale='log')
+ax.set_xlim([1, MAX_SNR])
+for xs, ys in greenTests:
+    ax.plot(xs, ys, color='green', lw=0.2)
+for xs, ys in redTests:
+    ax.plot(xs, ys, color='red', lw=0.2)
 
 
-    ax.set_xlabel('Отношение сигнал-шум (дБ)')
-    ax.set_ylabel('Вероятность ошибки на блок')
-    #plt.rc('grid', linestyle="-", color='black')
-    plt.grid(True)
+ax.set_xlabel('Отношение сигнал-шум (дБ)')
+ax.set_ylabel('Вероятность ошибки на блок')
+#plt.rc('grid', linestyle="-", color='black')
+plt.grid(True)
 
-    if args.operation == DRAW:
-        plt.show()
-    else:
-        plt.savefig('../images/' + filename + '.pdf')
-        plt.savefig('../images/' + filename + '.eps')
+if args.operation == DRAW:
+    plt.show()
+else:
+    plt.savefig('../images/' + filename + '.pdf')
+    plt.savefig('../images/' + filename + '.eps')
